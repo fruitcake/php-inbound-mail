1799148f2f6f8e4109e995be6849c389c1d3d81c
Barry vd. Heuvel <barryvdh@gmail.com>
2019-01-22 16:29:37 +0100

    Update tests

--- tests/InboundMailTest.php
+++ tests/InboundMailTest.php
@@ -26,5 +26,37 @@ class InboundMailTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($message->getFrom(), $reply->getTo());
         $this->assertEquals($message->getCc(), $reply->getCc());
         $this->assertEquals([$message->getId()], $reply->getHeaders()->get('In-Reply-To')->getFieldBodyModel());
+        $this->assertEquals([$message->getId()], $reply->getHeaders()->get('References')->getFieldBodyModel());
+    }
+
+    /**
+     * Test our example Parser, to check the Abstract Parser methods
+     */
+    public function testReplyWithReferences()
+    {
+        $message = (new \Swift_Message('Testmail'));
+        $message->setId('2@foo');
+        $message->getHeaders()->addIdHeader('References', '1@foo');
+
+        $inboundMail = new InboundMail();
+
+        $reply = $inboundMail->createSwiftReply($message);
+
+        $this->assertEquals(['1@foo', '2@foo'], $reply->getHeaders()->get('References')->getFieldBodyModel());
+    }
+
+
+    /**
+     * Test our example Parser, to check the Abstract Parser methods
+     */
+    public function testReplyWithRe()
+    {
+        $message = (new \Swift_Message('Re: Testmail'));
+        
+        $inboundMail = new InboundMail();
+
+        $reply = $inboundMail->createSwiftReply($message);
+
+        $this->assertEquals('Re: Testmail', $reply->getSubject());
     }
 }
